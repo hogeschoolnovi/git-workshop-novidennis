64955e83f8ab540a5620c7d0371f644b3bb9cce8
Dennis Haverhals <dennis.haverhals@noviuniversity.com>
2020-06-02 11:35:00 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello Dennis");
+        int getal = 0;
+        //type          naam              //waarde -> het object
+        LoginController loginController = new LoginController();
     }
 }
